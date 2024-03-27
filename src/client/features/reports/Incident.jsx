1b0987d6e1967cda9bf66c941faaa304d014cea0
Orion Cable <noiro.elbac@gmail.com>
2024-03-27 13:05:49 -0400

    added border and styling to iincident page

--- src/client/features/reports/Incident.jsx
+++ src/client/features/reports/Incident.jsx
@@ -30,11 +30,10 @@ export default function Incident() {
             className="card w-90vw bg-base-100 shadow-lg m-2.5"
             key={incident.id}
           >
-
             <div className="card-body">
-
-
-              <h2 className="text-3xl font-bold dark:text-white m-2.5 text-center">Incident Report Details</h2>
+              <h2 className="text-3xl font-bold dark:text-white m-2.5 text-center">
+                Incident Report Details
+              </h2>
 
               {/* <div className="flex">
                 <div class="border-2 border-yellow-400 text-yellow-400 rounded-md w-20 text-center">
@@ -42,28 +41,45 @@ export default function Incident() {
                 </div>
               </div> */}
 
-              <section class=".incident-section">
-                <h2>Product Details</h2>
+              <section class="incident-section">
+                <h2 class="incident-section-title">Product Details</h2>
+            
+                <p><strong>Product Description:</strong> {incident.productDescription}</p>
+                <p><strong>Manufacturer Name:</strong> {incident.manufacturer}</p>
+                <p><strong>Brand Name:</strong> {incident.brand}</p>
+                <p><strong>Model Name or Number:</strong> {incident.model}</p>
+                <p><strong>Serial Number:</strong> {incident.serialNumber}</p>
+                <p><strong>UPC Code:</strong> {incident.upc}</p>
+                <p><strong>Retailer:</strong> {incident.retailer}</p>
+                <p><strong>Reatailer State:</strong> {incident.state}</p>
+                <p><strong>Purchase Date:</strong> {incident.purchaseDate}</p>
+                <p><strong>Product Category</strong> {incident.productCategory}</p>
+                <p><strong>Product Type:</strong> {incident.productType}</p>
+                <p><strong>Product Code:</strong> {incident.productCode}</p>
+ 
+   
               </section>
-              
 
+              <section class="incident-section">
+                <h2 class="incident-section-title">Incident Details</h2>
+                <p><strong>Incident Description:</strong> {incident.incidentDescription}</p>
+                <p><strong>Report Date:</strong> {incident.reportDate}</p>
+                <p><strong>Incident State:</strong> {incident.state}</p>
+              </section>
 
+              <section class="incident-section">
+                <h2 class="incident-section-title">Victims Involved</h2>
+                <p><strong>Victim Gender:</strong> {incident.victimGender}</p>
+                <p><strong>Victim Age:</strong> {incident.victimAge}</p>
+              </section>
 
-              <h2>Incident Details</h2>
-              <h2>Victims Involved</h2>
-              <h2>Comments from Manufacturer</h2>
-              <h2>Aditional Details</h2>
-
-
-              
-              <p>{incident.productDescription}</p>
-
-
+              <section class="incident-section">
+                <h2 class="incident-section-title">Comments from Manufacturer</h2>
+                <p>{incident.companyComment}</p>
+              </section>
 
 
               {/* <div className="card-actions justify-end"></div> */}
-
-
             </div>
           </div>
         </>
