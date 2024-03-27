5acc366d6e7a6b3217149687a42027af8753a256
Orion Cable <noiro.elbac@gmail.com>
2024-03-27 12:07:17 -0400

    started formatting

--- src/client/features/reports/Incident.jsx
+++ src/client/features/reports/Incident.jsx
@@ -25,20 +25,45 @@ export default function Incident() {
           >
             Go Back
           </Link>
+
           <div
             className="card w-90vw bg-base-100 shadow-lg m-2.5"
             key={incident.id}
           >
-            <div className="card-body">
-              <h2 className="card-title">{incident.brand}</h2>
 
-              <div className="flex">
+            <div className="card-body">
+
+
+              <h2 className="text-3xl font-bold dark:text-white m-2.5 text-center">Incident Report Details</h2>
+
+              {/* <div className="flex">
                 <div class="border-2 border-yellow-400 text-yellow-400 rounded-md w-20 text-center">
                   Incident
                 </div>
-              </div>
+              </div> */}
+
+              <section class=".incident-section">
+                <h2>Product Details</h2>
+              </section>
+              
+
+
+
+              <h2>Incident Details</h2>
+              <h2>Victims Involved</h2>
+              <h2>Comments from Manufacturer</h2>
+              <h2>Aditional Details</h2>
+
+
+              
               <p>{incident.productDescription}</p>
-              <div className="card-actions justify-end"></div>
+
+
+
+
+              {/* <div className="card-actions justify-end"></div> */}
+
+
             </div>
           </div>
         </>
