a457f8ea418a2d8b36bcc2ca38ab5a27e3226afa
otto18 <otto@ceramic.global>
2024-04-04 11:54:26 -0400

    postgresdb setup

--- src/server/prisma/migrations/20240318195554_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[title]` on the table `Recall` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Recall_title_key" ON "Recall"("title");
--- src/server/prisma/migrations/20240318202002_/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "Recall_title_key";
